5d67f1183edc75d8ae73770d541854c045533226
Sabrina Spruck <113223660+SabrinaS0909@users.noreply.github.com>
2025-03-14 19:08:02 -0400

    button click dynamically changes text

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, request, jsonify
+from app.utils import get_animal_choices
 
 @app.route('/')
 def index():
@@ -8,8 +9,8 @@ def index():
 @app.route('/animal_click', methods = ['POST'])
 def animal_click():
     data = request.get_json()
-    animal = data['animal']
-    print(f"Server received: {animal}")
-    return jsonify ({"message": "Animal click received"})
+    player_action, computer_action = get_animal_choices(data)
+    print(f"It's {player_action} vs {computer_action}!!")
+    return jsonify ({"player": player_action, "computer": computer_action})
 
 print("routes is working")
--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,9 @@
-#for reusable logic and functions; define them here then put it in your code via the run.py file
+#for reusable logic and functions; define them here
+
+import random
+
+def get_animal_choices(data):
+    player_action = data["animal"]
+    possible_actions = ["bee", "bun", "corvid", "cat", "wolf", "human"]
+    computer_action = random.choice(possible_actions)
+    return player_action, computer_action
